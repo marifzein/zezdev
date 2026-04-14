c535fa87f165cc4d0c7935977332bdf6091bdc6e
M Arif Zein <marifzein@gmail.com>
2026-04-14 10:29:14 +0700

    disable rgt clck f12

--- script.js
+++ script.js
@@ -25,5 +25,24 @@ document.onkeydown = function (e) {
   if (e.keyCode == 123) return false; // F12
   if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) return false; // Ctrl+Shift+I
   if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) return false; // Ctrl+Shift+C
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) return false; // Ctrl+Shift+J
   if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) return false; // Ctrl+U (View Source)
 };
+
+// Efek peringatan ala Facebook di Console
+(function () {
+  const title = "BERHENTI!";
+  const message =
+    "Ini adalah fitur browser yang ditujukan untuk pengembang. Jika seseorang meminta Anda menyalin dan menempelkan sesuatu di sini untuk mengaktifkan fitur atau 'meretas' akun seseorang, itu adalah penipuan.";
+  const warning = "Menempelkan kode di sini dapat memberikan akses kepada penipu ke akun email/medsos anda.";
+
+  // Gaya teks Judul (Besar & Merah)
+  const titleStyle = "color: red; font-size: 50px; font-weight: bold; -webkit-text-stroke: 1px black;";
+
+  // Gaya teks Pesan (Hitam/Putih Standar tapi agak besar)
+  const messageStyle = "font-size: 18px;";
+
+  console.log("%c" + title, titleStyle);
+  console.log("%c" + message, messageStyle);
+  console.log("%c" + warning, "font-size: 18px; font-weight: bold; color: red;");
+})();
