7a0329a94672e86beebe0ee9cc5ba3f81cfbec79
M Arif Zein <marifzein@gmail.com>
2026-04-14 08:42:52 +0700

    disable rgt clck f12

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ document.querySelectorAll("#sideMenu a").forEach((link) => {
     sideMenu.style.left = "-100%";
   });
 });
+
+// Disable Klik Kanan
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+// Disable Shortcut F12, Inspect Element, dll
+document.onkeydown = function (e) {
+  if (e.keyCode == 123) return false; // F12
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) return false; // Ctrl+Shift+I
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) return false; // Ctrl+Shift+C
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) return false; // Ctrl+U (View Source)
+};
